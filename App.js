8d6f88ad2d0dc354fb3ec5aa68558a754666f68a
Jonathan Orlen <pellondou.jo@gmail.com>
2020-12-09 21:11:09 +0700

    add express js

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-const http = require('http');
+// Build using http
+// const http = require('http');
 
-const server = http.createServer((req,res) => {
-     res.end('hello world');
-})
-server.listen(3000, () => {
-     console.log('listening on port 3000')
-})
+// const server = http.createServer((req,res) => {
+//      res.end('hello world');
+// })
+// server.listen(3000, () => {
+//      console.log('listening on port 3000')
+// })
+
+const express = require('express')
+const app = express()
+const port = 2000
+
+app.get('/user', (req, res) => res.send("Hello User"))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost/${port}`))

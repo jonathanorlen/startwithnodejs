b59bae82d1b2692fe17d62dde9c07f20167ce0ce
Jonathan Orlen <pellondou.jo@gmail.com>
2020-12-09 21:04:48 +0700

    install chalk, OS, Make Server

--- hello.js
+++ hello.js
@@ -1,6 +1,12 @@
-const fs = require('fs');
+// const fs = require('fs');
+// const chalk = require('chalk');
+// const until = require('./until');
 
-const name = require('./until');
+// fs.writeFileSync('note.txt', until.name);
+// fs.appendFileSync('note.txt', until.sum(1,2));
 
-fs.writeFileSync('note.txt', name);
-fs.appendFileSync('note.txt', 'Append');
+// console.log(chalk.red.bold(until.name))
+
+// OS Module
+// const os = require("os");
+// console.log(os.cpus())
--- until.js
+++ until.js
@@ -1,6 +1,12 @@
 let name = "Jonathan Orlen"
 let total = 20
+
+let sum = (x,y) => {
+     return x + y;
+}
+
 module.exports = {
      total,
-     name
+     name,
+     sum
 };
